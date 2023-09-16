20c735cd5305966cfceefa0058f5c12202a47c48
mohammad dehghanifard <104780008+mohammad-dehghanifard@users.noreply.github.com>
2023-09-16 05:06:20 -0700

    impl appColors class

--- lib/componnets/app_colors.dart
+++ lib/componnets/app_colors.dart
@@ -1,3 +1,9 @@
+import 'dart:ui';
+
 class AppColors{
+
+  static Color lightBackGroundColor = const Color(0xffFFFFFF);
+  static Color lightTextFieldColor = const Color(0xffF0F0F0);
+  static Color lightBtnColor = const Color(0xffA73892);
 
 }
